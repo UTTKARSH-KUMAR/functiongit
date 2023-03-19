37bbdc39893c239817a3f7ce8a1d6f14b8f68ea5
uttkarshthakur0325@gmail.com <uttkarshkumar@UTTKARSHs-MacBook-Air.local>
2023-03-19 09:35:42 +0530

    function is being added

---
+++ power_fnc.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int power(int a,int b){
+    int ans=1;
+    for(int i=1;i<=b;i++)
+    {
+        ans=ans*a;
+    }
+    return ans;
+}
+int main()
+{   int a,b;
+    cin>> a >> b;
+    int answere=power(a,b);
+    cout<<"the answere is "<<answere<<endl;
+    return answere;
+
+
+}
